6fa3b2128dee60d4e6eec9fa2bb51f63916f174d
Andrei Hutsau <mwplt@mail.ru>
2022-05-28 11:14:46 +0300

    * after login everything

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,15 @@ function App() {
         <h2>
             <NavLink to={'/'}>main</NavLink>---
             <NavLink to={'/login'}>login</NavLink>---
-            <NavLink to={'/profile'}>profile</NavLink>
+            <NavLink to={'/profile'}>profile</NavLink>---
+            <NavLink to={'/profile/settings'}>settings</NavLink>
         </h2>
         <Routes>
+            <Route path={'/*'} element={<h1>404 Page not found</h1>}/>
             <Route path={'/'} element={<h1>main</h1>}/>
-            <Route path={'/login'} element={<h1>login</h1>}/>
+            <Route path={'/login/*'} element={<h1>login</h1>}/>
             <Route path={'/profile'} element={<h1>profile</h1>}/>
+            <Route path={'/profile/settings'} element={<h1>settings</h1>}/>
         </Routes>
     </div>
   );
