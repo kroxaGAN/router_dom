6244921d2be38baba096309c51c9ee36c987f5c0
Andrei Hutsau <mwplt@mail.ru>
2022-05-28 11:08:52 +0300

    make some routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {NavLink, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h3>REACT ROUTER DOM V6</h3>
+        <h2>
+            <NavLink to={'/'}>main</NavLink>---
+            <NavLink to={'/login'}>login</NavLink>---
+            <NavLink to={'/profile'}>profile</NavLink>
+        </h2>
+        <Routes>
+            <Route path={'/'} element={<h1>main</h1>}/>
+            <Route path={'/login'} element={<h1>login</h1>}/>
+            <Route path={'/profile'} element={<h1>profile</h1>}/>
+        </Routes>
     </div>
   );
 }
