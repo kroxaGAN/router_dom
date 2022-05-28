fe6ecb0249d585d9379661d973fcb3d7e2368971
Andrei Hutsau <mwplt@mail.ru>
2022-05-28 11:23:01 +0300

    vlozennosti settings at profile

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,31 @@ import './App.css';
 import {NavLink, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-     <h3>REACT ROUTER DOM V6</h3>
-        <h2>
-            <NavLink to={'/'}>main</NavLink>---
-            <NavLink to={'/login'}>login</NavLink>---
-            <NavLink to={'/profile'}>profile</NavLink>---
-            <NavLink to={'/profile/settings'}>settings</NavLink>
-        </h2>
-        <Routes>
-            <Route path={'/*'} element={<h1>404 Page not found</h1>}/>
-            <Route path={'/'} element={<h1>main</h1>}/>
-            <Route path={'/login/*'} element={<h1>login</h1>}/>
-            <Route path={'/profile'} element={<h1>profile</h1>}/>
-            <Route path={'/profile/settings'} element={<h1>settings</h1>}/>
-        </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h3>REACT ROUTER DOM V6</h3>
+            <h2>
+                <NavLink to={'/'}>main</NavLink>---
+                <NavLink to={'/login'}>login</NavLink>---
+                <NavLink to={'/profile'}>profile</NavLink>---
+                <NavLink to={'/profile/settings'}>settings</NavLink>
+            </h2>
+            <Routes>
+                <Route path={'/*'} element={<h1>404 Page not found</h1>}/>
+                <Route path={'/'} element={<h1>main</h1>}/>
+                <Route path={'/login/*'} element={<h1>login</h1>}/>
+                <Route path={'/profile/*'} element={(
+                            <h1>
+                                profile
+                                <Routes>
+                                <Route path={'/settings'} element={<h3>settings</h3>}/>
+                                </Routes>
+                            </h1>
+                    )}
+                />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
