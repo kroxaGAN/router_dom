e7bdc5e6c2104f6ca4dc3c93b77e689ac8fcf59b
Andrei Hutsau <mwplt@mail.ru>
2022-05-29 08:33:40 +0300

    outlet knew

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {NavLink, Route, Routes} from "react-router-dom";
+import {NavLink, Outlet, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
@@ -10,21 +10,37 @@ function App() {
                 <NavLink to={'/'}>main</NavLink>---
                 <NavLink to={'/login'}>login</NavLink>---
                 <NavLink to={'/profile'}>profile</NavLink>---
-                <NavLink to={'/profile/settings'}>settings</NavLink>
+                <NavLink to={'/profile/settings'}>settings</NavLink>---
+                {/*<NavLink to={'/profile/set'}>set</NavLink>*/}
+                <NavLink to={'/my'}>my</NavLink>---
+                <NavLink to={'/my/set'}>mySet</NavLink>----
+                <NavLink to={'/my/set2'}>mySet2</NavLink>
             </h2>
             <Routes>
                 <Route path={'/*'} element={<h1>404 Page not found</h1>}/>
                 <Route path={'/'} element={<h1>main</h1>}/>
-                <Route path={'/login/*'} element={<h1>login</h1>}/>
+                <Route path={'/login'} element={<h1>login</h1>}/>
                 <Route path={'/profile/*'} element={(
                             <h1>
                                 profile
                                 <Routes>
                                 <Route path={'/settings'} element={<h3>settings</h3>}/>
+
                                 </Routes>
                             </h1>
                     )}
                 />
+                <Route path={'/my'} element={(
+                    <h4>
+                        my
+                        <Outlet/>
+                    </h4>
+                )}>
+                    <Route path={'/my/set'} element={<h3>set</h3>}/>
+                    <Route path={'/my/set2'} element={<h3>set2</h3>}/>
+                </Route>
+
+
             </Routes>
         </div>
     );
